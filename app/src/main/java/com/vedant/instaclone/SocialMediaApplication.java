5abfbe83db86e89faf24e3d6fa326e402e4fa31b
Vedant Tiwari <tiwarivedant.1690@gmail.com>
2021-10-07 23:46:08 +0530

    problem solved

--- app/src/main/java/com/vedant/instaclone/SocialMediaApplication.java
+++ app/src/main/java/com/vedant/instaclone/SocialMediaApplication.java
@@ -1,14 +1,37 @@
 package com.vedant.instaclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+
+import com.google.android.material.tabs.TabLayout;
+import com.vedant.instaclone.Adapters.TabAdapters;
+
 public class SocialMediaApplication extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapters tabAdapters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media_application);
+
+        setTitle("Social Media App!!!");
+
+        toolbar = findViewById(R.id.mytoolbar);
+         setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapters = new TabAdapters(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapters);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
     }
 }
