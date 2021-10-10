48b4e570d23340e3cb54764715db76b117ca7607
Vedant Tiwari <tiwarivedant.1690@gmail.com>
2021-10-10 13:01:18 +0530

    user POst

---
+++ app/src/main/java/com/vedant/instaclone/userPost.java
@@ -0,0 +1,14 @@
+package com.vedant.instaclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_post);
+    }
+}
